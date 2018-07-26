0b0fe0015c951d9e7963ec43e6d0c53f86b97884
diamondcoder <idawebqueen@gmail.com>
2018-07-27 00:31:42 +0200

    youtube api integration and getting data

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component{
+
+constructor(props){
+
+  super(props);
+
+    this.state= {term: 'starting value'}
+
+}
+render() {
+
+
+  return (
+    <div>
+
+
+    <input value={this.state.term} onChange={event=>this.setState({term:event.target.value})} />
+     
+     </div>
+  );
+
+}
+
+/*
+onInputChange(event){
+  console.log(event.target.value);
+}*/
+
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+const API_KEY = 'AIzaSyDI5rmJvKNYVairYgC2_vQ49w1-c7IkFlk';
+
+
+
+class App extends Component {
+constructor(props){
+
+  super(props);
+
+  this.state={videos:[] };
+
+  YTSearch({
+
+            key:API_KEY,
+            term:'surfboards'},
+            (videos)=>{
+            //this.setState({videos:videos})
+            this.setState({videos})
+
+            });
+
+}
+  render(){
+    return (
+      <div>
+         <SearchBar />
+
+         <VideoDetail video={this.state.videos[0]} />
+
+         <VideoList videos={this.state.videos} />
+       </div>
+    );
+
+
+  }
+
+
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
